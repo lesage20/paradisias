cf6c5bfcffbdbc25c3bfb9386e48b5c64f985e8f
lesage20 <angezanou00@gmail.com>
2022-06-19 03:09:14 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,4 +9,8 @@
 import { provide } from "@vue/runtime-core";
 
 provide("api", "https://paradisias-api.herokuapp.com/");
+<<<<<<< Updated upstream
+=======
+// provide("api", "http://192.168.43.108:8000/");
+>>>>>>> Stashed changes
 </script>
